7910e185648f02a9989c9c791603b5a5db120aca
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-12-05 12:59:23 +0100

    README Updated, New Library Project, Program.cs Updated Ex2 Complet

--- projecteProva2/projecteProva2/Program.cs
+++ projecteProva2/projecteProva2/Program.cs
@@ -1,10 +1,66 @@
 ﻿namespace projecteProva2
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProgramCore();
+        }
+        public static void ProgramCore()
+        {
+            int[] arr = { 10, -4, 6, 4, 8, 13, 2, -4 };
+            bool inArray = SearchNum(arr);
+
+            Console.WriteLine("Està a la array: " + inArray);
+            PrintArray(arr);
+        }
+        public static bool SearchNum(int[] arr)
+        {
+            const string MsgInpSerchNum = "Introdueix el nombre a buscar:";
+
+            Console.WriteLine(MsgInpSerchNum);
+            int num = ReadUserInput();
+            int arrPosition = 0;
+            
+            SortArray(arr);
+            arrPosition = ex2_llibreries.SearchClass.BinarySearch(arr, 0, arr.Length-1, num);
+
+            if (arrPosition == -1)
+            {
+                return false;
+            } else if (num == arr[arrPosition])
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void SortArray(int[] arr)
+        {
+            ex2_llibreries.SecondSort.Order(arr, 0, arr.Length - 1);
+        }
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        public static int ReadUserInput()
+        {
+            const string ErrException = "ERROR INESPERAT: Valor mal introduït";
+
+            int num = 0;
+
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ErrException);
+                num = 1;
+            }
+            return num;
         }
     }
 }
